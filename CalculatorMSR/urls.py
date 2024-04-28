ea17d14c69c5dac6126c79c20120a88a3e6650da
DenysParzh <denisssporzh@gmail.com>
2024-04-28 14:54:24 +0300

    Rename project

--- CalculatorMSR/urls.py
+++ CalculatorMSR/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for CalculatorAMPM project.
+URL configuration for CalculatorMSR project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- CalculatorMSR/wsgi.py
+++ CalculatorMSR/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CalculatorAMPM project.
+WSGI config for CalculatorMSR project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CalculatorAMPM.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CalculatorMSR.settings')
 
 application = get_wsgi_application()
